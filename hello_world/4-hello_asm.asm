d0d94db3396c546a8945abf665f8bae93c7e2dae
Mason <mason.e.fish@gmail.com>
2016-01-31 22:49:23 +0000

    upload own asm

--- hello_world/4-hello_asm.asm
+++ hello_world/4-hello_asm.asm
@@ -1,18 +1,20 @@
-global main
-
 section .data                  
-	str: db "Hello, asm", 0xa
-	len: equ $-str
 
-section .text
-	main:
+str:	db "Hello, asm", 0xa
+len	equ $-str
+
 	
-	mov edx, len
-	mov ecx, str
-	mov ebx, 2
-	mov eax, 4
-	int 0x80
+section .text
+global main
+	
+main:
+	
+	mov	edx, len
+	mov	ecx, str
+	mov	ebx, 2
+	mov	eax, 4
+	int	0x80
 
-        mov ebx, 0
-        mov eax, 1
-        int 0x80
+        mov	ebx, 0
+        mov	eax, 1
+        int	0x80
