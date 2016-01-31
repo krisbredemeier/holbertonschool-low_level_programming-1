72a354da564a635b16f1f9cafd29125aea634211
Mason <mason.e.fish@gmail.com>
2016-01-31 22:31:07 +0000

    upload own asm file

--- hello_world/4-hello_asm.asm
+++ hello_world/4-hello_asm.asm
@@ -1,19 +1,18 @@
-section .data                   ; data section
-	;;  the string to print, 0xa=cr
-msg:	    db "Real programmers can write assembly code in any language", 0xa, "- Larry Wall", 0xa
-	len     equ $-msg	; $ means "here"
-	;;  len is a value, not an address
+global main
 
-	section .text		; code section
-	global main		; make label available to linker
+section .data                  
+	str: db "Hello, asm", 0xa
+	len: equ $-str
 
-main:				; standard gcc entry point
-	        mov     edx, len ; arg3, length of string to print
-	        mov     ecx, msg ; arg2, address of the string
-	        mov     ebx, 2	 ; arg1, where to write, standard error
-	        mov     eax, 4	 ; system call number (sys_write)
-	        int     0x80	 ; interrupt 80 hex, call kernel
+section .text
+	main:
+	
+	mov edx, len
+	mov ecx, str
+	mov ebx, 2
+	mov eax, 4
+	int 0x80
 
-	        mov     ebx, 98	; exit code, 0=normal
-	        mov     eax, 1	; system call number (sys_exit)
-	        int     0x80	; interrupt 80 hex, call kernel
+        mov ebx, 0
+        mov eax, 1
+        int 0x80
