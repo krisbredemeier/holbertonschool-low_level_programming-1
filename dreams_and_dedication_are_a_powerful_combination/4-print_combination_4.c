f0c24bc7fad160c71e19dc9914bc2ded85c1ba81
Mason Fish <mason.e.fish@gmail.com>
2016-02-11 02:26:59 +0000

    include comments on 5-...

--- dreams_and_dedication_are_a_powerful_combination/4-print_combination_4.c
+++ dreams_and_dedication_are_a_powerful_combination/4-print_combination_4.c
@@ -1,8 +1,10 @@
 #include "my_functions.h"
 
-/* 
- *
- */
+/*
+ * [print_combination_4] CYCLES THROUGH TWO 2-DIGIT NUMBERS, INCREMENTING 
+ * EACH FROM THE RIGHT TO THE LEFT, WHILE UPDATING A REFERENCE INDEX TO
+ * RESET TO SO THAT IT WILL NOT REPEAT NUMBERS AND REMAIN CONSECUTIVE.
+*/
 
 void print_combination_4(void) {
   int x = 0, y = 1, next = 2;
