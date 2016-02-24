86546580bc3d012511de742ce691e1e1aadf4f7b
Mason Fish <41@p1-6.hbtn.io>
2016-02-23 16:38:27 -0800

    remove c++ style comments, improve print_number

--- i_m_not_in_the_loop/1-print_alphabet.c
+++ i_m_not_in_the_loop/1-print_alphabet.c
@@ -1,8 +1,9 @@
 #include "my_functions.h"
 
-// print_alphabet increments through the ASCII characters, starting 
-// at the char 'a', until it exceeds the char 'z' and uses 
-// print_char to print each iteration.
+/* print_alphabet increments through the ASCII characters, starting 
+ * at the char 'a', until it exceeds the char 'z' and uses 
+ * print_char to print each iteration.
+ */
 void print_alphabet(void) {
   char letter = 'a';
   while(letter <= 'z') {
--- i_m_not_in_the_loop/5-print_number.c
+++ i_m_not_in_the_loop/5-print_number.c
@@ -1,62 +1,29 @@
 #include "my_functions.h"
-#define MAX_LENGTH 100
-
 
 /*
-print_number takes an int, and first passes it to check_number                                           
-to determine if it is 0, a negative number, or the minimum. print_number then                            
-loops through the int dividing it by 10 until it equals 0, and                                           
-indexes it's iterations. This determines how many digits the                                             
-original int is. It then loops through each digit, uses the modulus                                      
-to determine what that digit is, and fills a char array with the ASCII                                   
-char number equivalent to that int. Finally it gives the array to                                        
-the function give_char which prints out each array index.   
+ *[print_number] TAKES AN INT, DIVIDES IT BY 10 AS MANY TIMES AS IT CAN TO
+ *DETERMINE HOW MANY DIGITS IT CONTAINS, AND THEN CONVERTS EACH DIGIT TO A SINGLE
+ *CHAR BY USING THE MODULUS OPERATOR AND ADDING THIS VALUE TO THE CHAR '0'. FINALLY
+ *THIS CHAR IS PASSED TO THE [print_char] FUNCTION.
  */
 
-int check_number(int num);
-void give_char(char str[], int len);
-
-void print_number(int num) {
-  if ((num = check_number(num))) {
-    char str[MAX_LENGTH];
-    int i, rem, len = 0, n = num;
-    while (n != 0) {
-	len++;
-	n /= 10;
-    }
-    for (i = 0; i < len; i++) {
-	rem = num % 10;
-	num = num / 10;
-	str[len - (i + 1)] = rem + '0';
-    }
-    str[len] = '\0';
-    give_char(str, len);
-  }
-}
-
-void give_char(char str[], int len) {
-  int i = 0;
-  while(i < len) {
-    print_char(str[i++]);
-  }
-}
-
-int check_number(int num) {
-  if (num == -2147483648) {
-    give_char("-2147483648", 12);
-    return 0;
-  }
-  else if (num < 0){
-    num *= -1;
+void print_number(int n) {
+  int i = 1;
+  int temp;
+  int mod = 1;
+  if (n < 0) {
     print_char('-');
-    return num;
+    mod = -1;
   }
-  if (num == 0) {
-    print_char('0');
-    return 0;
+  for(temp = n; temp > 9 || temp < -9; temp = temp / 10) {
+    i++;
   }
-  else {
-    return num;
+  while(i) {
+    int i2 = i--;
+    temp = n;
+    while(i2-- > 1) {
+      temp /= 10;
+    }
+    print_char(((temp % 10) * mod + '0'));
   }
 }
-
