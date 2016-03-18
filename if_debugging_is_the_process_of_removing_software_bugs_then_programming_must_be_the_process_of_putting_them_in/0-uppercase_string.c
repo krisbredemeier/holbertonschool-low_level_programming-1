3448d8647d366684ba8001786afa46159dbc8ba8
Mason <mason.e.fish@gmail.com>
2016-03-18 03:27:24 +0000

    first commit for proper file and directory names

---
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/0-uppercase_string.c
@@ -0,0 +1,11 @@
+char *uppercase_string(char *c) {
+  int i;
+
+  for (i = 0; c[i]; i++) {
+    if (c[i] > 96 && c[i] < 123) {
+      c[i] -= 32;
+    }
+  }
+
+  return c; 
+}
---
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/2-1337.c
@@ -0,0 +1,13 @@
+char *leet(char *c) {
+  int i;
+
+  for (i = 0; c[i]; i++) {
+    /* USE SWITCH */
+    switch c[i] {
+	???syntax???
+      
+    case a
+  }
+
+  return c;
+}
