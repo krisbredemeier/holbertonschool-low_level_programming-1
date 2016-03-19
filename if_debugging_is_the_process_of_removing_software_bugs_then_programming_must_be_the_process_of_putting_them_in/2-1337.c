89004f541a4a8cc4f41c9087716f2ea5ffc79bba
Mason <mason.e.fish@gmail.com>
2016-03-19 05:59:33 +0000

    finished task 4

--- if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/2-1337.c
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/2-1337.c
@@ -2,11 +2,29 @@ char *leet(char *c) {
   int i;
 
   for (i = 0; c[i]; i++) {
-    /* USE SWITCH */
-    switch c[i] {
-	???syntax???
-      
-    case a
+    
+    switch(c[i]) {  
+      case 'a':
+      case 'A':
+        c[i] = '4';
+        break;
+      case 'e':
+      case 'E':
+        c[i] = '3';
+        break;
+      case 'o':
+      case 'O':
+        c[i] = '0';
+        break;
+      case 't':
+      case 'T':
+        c[i] = '7';
+        break;
+      case 'l':
+      case 'L':
+        c[i] = '1';
+        break;
+    }
   }
 
   return c;
