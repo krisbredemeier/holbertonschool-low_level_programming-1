74d9b865d81b0fa2ce8850bd20634e5d71207288
Mason <mason.e.fish@gmail.com>
2016-05-09 07:11:09 +0000

    clean up code for task 5

--- linked_lists/5-rev_list.c
+++ linked_lists/5-rev_list.c
@@ -2,35 +2,18 @@
 #include <stdlib.h>
 #include "list.h"	
 
-int get_size_of_list(List *);
-
+/* rev_list() reverses a linked list's node order */
 void rev_list(List **list) {
-  List *prev   = NULL;
-  List *current = *list;
-  List *next;
-  while (current != NULL)
-    {
-      next = current->next;  
-      current->next = prev;   
-      prev = current;
-      current = next;
-    }
+  List *prev, *current, *next;
+  prev = NULL;
+  current = *list;
+
+  while (current != NULL) {
+    next = current->next;  
+    current->next = prev;   
+    prev = current;
+    current = next;
+  }
 
   *list = prev;
-}
-
-int get_size_of_list(List *sPtr) {
-  int i;
-  
-  if (sPtr == NULL) {
-    return (0);
-  }
-  
-  i = 1;
-  while(sPtr->next != NULL) {
-    sPtr = sPtr->next;
-    i++;
-  }
-
-  return i;
 }
